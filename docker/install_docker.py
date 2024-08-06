3428121a3672c58a20c20e54da6bca420a16cf5f
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2024-08-06 18:55:39 +0100

    added -y flag to apt-get update commands

--- docker/install_docker.py
+++ docker/install_docker.py
@@ -4,15 +4,17 @@ import os
 
 def ubuntu_install():
     if os.system("sudo docker images") != 0:
-        print("starting docker install")
+        print("starting docker install by running apt-get update first")
         print("#####################################")
-        print("add docker official GPG key")
         os.system("sudo apt-get update -y")
         os.system("sudo apt-get install ca-certificates curl")
+       
+        print("Now adding docker official GPG key")
+        print("#####################################")
         os.system("sudo install -m 0755 -d /etc/apt/keyrings")
         os.system("sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc")
         os.system("sudo chmod a+r /etc/apt/keyrings/docker.asc")
-    
+      
         print("add repository to apt sources")
         os.system("echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable\" |  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null")
         os.system("sudo apt-get update -y")
